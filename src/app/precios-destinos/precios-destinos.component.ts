bec6635545f48246ff83888d0397301e7aaf44e5
mathiso1805 <mathiso1805@gmail.com>
2021-10-05 20:46:23 -0300

    1 file modified

--- src/app/precios-destinos/precios-destinos.component.ts
+++ src/app/precios-destinos/precios-destinos.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Paquete } from '../paquete';
+import { PaquetesService } from '../paquetes.service';
+import { Venta } from '../venta';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle,
+} from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
 
 @Component({
   selector: 'app-precios-destinos',
@@ -7,9 +24,106 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreciosDestinosComponent implements OnInit {
 
-  constructor() { }
+  paquetes : Paquete [];
+  ventas : Venta [];
+  ventaPaqueteGroup;
+  venta: any;
+  paquete: any;
+  nombreDestinos : string [];
+  personasPorDestino : number[];
 
-  ngOnInit() {
+  @ViewChild('chart') chart!: ChartComponent;
+  public chartOptions!: Partial<ChartOptions> | any;
+
+
+  constructor(private paqueteService: PaquetesService) { 
+    this.getPaquetes();
+    this.getVentas();
+    this.nombreDestinos = [''];
+    this.personasPorDestino = [1];
+    
+    this.chartOptions = {
+      series: [
+        {
+          name: 'Personas',
+          data: this.personasPorDestino,
+          //[10, 41, 35, 51, 49]
+        },
+      ],
+      chart: {
+        height: 250,
+        type: 'bar',
+      },
+      title: {
+        text: 'Personas por Destino',
+      },
+      xaxis: {
+        categories: this.nombreDestinos,
+        //['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+      },
+      
+    };
   }
 
+  ngOnInit() {
+    this.ventas = [];
+    this.paquetes = [];
+    this.nombreDestinos = [''];
+    this.personasPorDestino = [0];
+
+    this.getPaquetes();
+    this.getVentas();
+    
+  }
+  getPaquetes(){
+    this.paqueteService.getPaquetesApi().subscribe((response)=> {
+    this.paquetes = response["destinos"];
+    this.nombreDestinos = this.paquetes.map(function(paquete) {
+        return paquete.nombre;
+      });
+    });
+   }
+
+
+   getVentas(){    
+     this.paqueteService.getVentas().subscribe((response)=> {
+       this.ventas = response["ventas"];      
+       this.paqueteService.setVentas(this.ventas);
+             //this.getPersonasPorDestino();      
+       for(var j=0; j < this.paquetes.length; j++){       
+         this.personasPorDestino[j] = 0;      
+        }      
+        for (var i=0; i< this.ventas.length; i++) {         
+          for (var j=0; j < this.paquetes.length; j++){                  
+            if (this.paquetes[j].id === this.ventas[i].id_paquete){
+              this.personasPorDestino[this.paquetes[j].id-1] += this.ventas[i].cantidad_mayores;this.personasPorDestino[this.paquetes[j].id-1] += this.ventas[i].cantidad_menores;          
+            }        
+          }      
+        }      
+        this.setChartOptions();      
+      });         
+    }
+
+    setChartOptions(){
+      this.chartOptions = {
+        series: [
+          {
+            name: 'Personas',
+            data: this.personasPorDestino,
+          },
+        ],
+        chart: {
+          height: 250,
+          type: 'bar',
+        },
+        title: {
+          text: 'Personas por Destino',
+        },
+        xaxis: {
+          categories: this.nombreDestinos,
+        },
+        
+      };
+    }
+    
 }
